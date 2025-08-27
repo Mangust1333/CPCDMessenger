cae7efb641f89f3fad9d0f967b020933898617e4
Vlad <Mangusten0k@yandex.ru>
2025-08-27 17:00:45 +0300

    Add: ChatService

---
+++ RelayServer/BusinessLayer/repository/IMessageRepository.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#include "../domain/Message.h"
+#include <boost/uuid/uuid.hpp>
+
+class IMessageRepository {
+public:
+    virtual ~IMessageRepository()=default;
+
+
+    virtual const boost::uuids::uuid& CreateMessage(const Message& message) = 0;
+    virtual std::optional<Message> GetById(const boost::uuids::uuid& id) = 0;
+    virtual std::vector<Message> GetByChatId(const boost::uuids::uuid& id) = 0;
+    virtual std::vector<Message> GetByNick(const std::string& nick) = 0;
+    virtual void UpdateMessage(const Message& user) = 0;
+    virtual void DeleteMessage(const boost::uuids::uuid& id) = 0;
+
+};
---
+++ RelayServer/BusinessLayer/services/ChatService/ChatService.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "IChatService.h"
+
+class ChatService : IChatService {
+public:
+
+
+};
